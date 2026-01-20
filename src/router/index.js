11594f8a147e975c7b835ffa4a27d9a4e15936d7
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-20 20:43:53 +0200

    feat: setup Vuex store skeleton

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from '../views/Home.vue';
+import Search from '../views/Search.vue';
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        component: Search
     }
 ]
 
