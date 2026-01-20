6740358133bafb737f5d7067781f880e0a668266
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-20 20:43:17 +0200

    feat: setup App layout with Navbar, Footer, and router-view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer
+  }
+}
 </script>
 
 <template>
-  <router-view />
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+
+    <router-view class="flex-1 flex flex-col bg-[#1a1a1a]" />
+
+    <Footer />
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
