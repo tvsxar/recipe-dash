4b85029957aa0770271646d7a9d5e2cf747e7249
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-22 17:39:11 +0200

    feat: add EmptyState & all components in search page

---
+++ src/components/EmptyState.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'EmptyState',
+    props: {
+        recipes: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="flex flex-col items-center text-center mt-16 text-gray-400">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mb-3" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1110.5 3a7.5 7.5 0 016.15 13.65z" />
+        </svg>
+
+        <p class="text-sm sm:text-base">
+            Start typing a recipe name or select a category to see results
+        </p>
+    </div>
+</template>
+
+<style></style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,28 @@
 <script>
+import SearchBar from '../components/SearchBar.vue';
+import CategoriesGrid from '../components/CategoriesGrid.vue';
+import EmptyState from '../components/EmptyState.vue';
 export default {
     name: 'Home',
+    components: {
+        SearchBar,
+        CategoriesGrid,
+        EmptyState
+    },
+    data() {
+        return {
+            searchQuery: '',
+            recipes: []
+        }
+    }
 }
 </script>
 
 <template>
-    <div>
-        Search Page
+    <div class="w-full mx-auto py-4 px-4 sm:px-12 lg:px-25">
+        <SearchBar />
+        <CategoriesGrid />
+        <EmptyState v-if="!searchQuery && recipes.length === 0" :recipes="recipes" />
     </div>
 </template>
 
