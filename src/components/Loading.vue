57fdf2b8eb986b3219ed3dd301a34329f8ba5268
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-22 13:48:57 +0200

    feat: add new Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "Loading",
+};
+</script>
+
+<template>
+    <div class="flex justify-center items-center py-10">
+        <div class="w-12 h-12 border-4 border-t-[#FFE08A] border-gray-700 rounded-full animate-spin"></div>
+    </div>
+</template>
+
+<style scoped></style>
