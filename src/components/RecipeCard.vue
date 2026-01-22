723a42c6cb1af49f4fc97a54e0f4d58c182c10bf
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-22 14:18:34 +0200

    style: responsiveness

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -14,11 +14,16 @@ export default {
     <div
         class="bg-[#1f1f1f] rounded-xl overflow-hidden shadow-lg hover:scale-101 transition transform cursor-pointer
            border border-gray-700 hover:border-[#FFE08A] hover:drop-shadow-[0_0_6px_rgba(255,224,138,0.6)] flex flex-col">
+
         <img :src="recipe.strMealThumb" :alt="recipe.strMeal" class="w-full h-40 object-cover" />
 
         <div class="p-4 flex flex-col flex-1">
-            <h3 class="text-white font-bold text-lg mb-2">{{ recipe.strMeal }}</h3>
-            <p class="text-gray-400 text-sm mt-auto">{{ recipe.strCategory }} • {{ recipe.strArea }}</p>
+            <!-- Заголовок адаптивный: маленький на мобилке, больше на больших экранах -->
+            <h3 class="text-white font-bold text-base sm:text-lg md:text-xl mb-2">{{ recipe.strMeal }}</h3>
+            <!-- Подпись тоже адаптивная -->
+            <p class="text-gray-400 text-xs sm:text-sm md:text-base mt-auto">
+                {{ recipe.strCategory }} • {{ recipe.strArea }}
+            </p>
         </div>
     </div>
 </template>
