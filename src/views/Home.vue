a17ca703af671f114ac33a4e9049f9a941752763
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-22 13:49:41 +0200

    style: centered Page by Y

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,29 @@
 <script>
-    import RecipesGrid from '../components/RecipesGrid.vue';
+import RecipesGrid from '../components/RecipesGrid.vue';
+import Loading from '../components/Loading.vue';
 export default {
     name: 'Home',
     components: {
-        RecipesGrid
+        RecipesGrid,
+        Loading
+    },
+    computed: {
+        loading() {
+            return this.$store.state.loading;
+        }
     }
 }
 </script>
 
 <template>
-    <div class="mx-auto py-4 px-4 sm:px-12 lg:px-25">
-        <RecipesGrid />
+    <div class="relative min-h-[80vh]">
+        <div v-if="loading" class="absolute inset-0 flex items-center justify-center">
+            <Loading />
+        </div>
+
+        <div v-else class="mx-auto py-4 px-4 sm:px-12 lg:px-25">
+            <RecipesGrid />
+        </div>
     </div>
 </template>
 
