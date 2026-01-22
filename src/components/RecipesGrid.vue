1ce736e3df7437fb148d80e5805a0473fd8d5131
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-22 19:30:39 +0200

    refactor: refactored Home page and Grid

--- src/components/RecipesGrid.vue
+++ src/components/RecipesGrid.vue
@@ -15,10 +15,6 @@ export default {
 
 <template>
     <div>
-        <h2 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-white mb-6">
-            Random Recipes
-        </h2>
-
         <div class="grid gap-6 md:gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             <RecipeCard v-for="recipe in recipes" :key="recipe.idMeal" :recipe="recipe" />
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,6 +30,9 @@ export default {
         </div>
 
         <div v-else class="mx-auto py-4 px-4 sm:px-12 lg:px-25">
+            <h2 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-white mb-6">
+                Recipes
+            </h2>
             <RecipesGrid :recipes="recipes" />
         </div>
     </div>
